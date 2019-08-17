e7dee0cf1a98558af2d80006c95c0ae8776416e8
Henrique Moreira de Albuquerque <henriquemoreira842@gmail.com>
2019-08-17 14:55:55 -0300

    feat: adicionando o Watson Natural Language understanting service no projeto

--- robots/text.js
+++ robots/text.js
@@ -2,10 +2,23 @@ const algorithmia = require('algorithmia');
 const apiKeyAlgorithmia = require('../credentials/algorithmia.json').apiKey
 const sbd = require('sbd')
 
+const watsonApiKey = require('../credentials/watson-nlu.json').apiKey
+var NaturalLanguageUnderstandingV1 = require('../node_modules/watson-developer-cloud/natural-language-understanding/v1.js');
+ 
+var nlu = new NaturalLanguageUnderstandingV1({
+    iam_apikey: watsonApiKey,
+    username: 'apikey',
+    password: 'RgoRxUvI7A29mgOE6XUq4_qVUU9f_E-XMuc4o3QzUZnK',
+    version: '2018-04-05',
+    url: 'https://gateway.watsonplatform.net/natural-language-understanding/api/'
+  })
+  
 async function robot(content){
     await fetchContentFromWikipedia(content)
     sanitizedContent(content)
     breakContentIntoSentences(content)
+    limitMaximumSentences(content)
+    await fetchKeywordsOfAllSentences(content)
 
     async function fetchContentFromWikipedia(content){
         const algorithmiaAutenticad = algorithmia(apiKeyAlgorithmia)
@@ -51,6 +64,39 @@ async function robot(content){
                 images: []
             })
         })
+    }
+
+    function limitMaximumSentences(content){
+        content.sentences = content.sentences.slice(0, content.maximumSentence)
+    }
+
+    async function fetchKeywordsOfAllSentences(content){
+        for(const sentence of content.sentences){
+            sentence.keywords = await fetchWatsonAndReturnKeywords(sentence.text)
+        }
+    }
+
+    async function fetchWatsonAndReturnKeywords(sentence){
+        return new Promise ((resolve, reject) =>{
+            nlu.analyze(
+            {
+                text: sentence,
+                features: {
+                    keywords: {}
+                }
+                },(error, response) =>{
+                    if (error) {
+                        throw error
+                    } 
+                    const keywords = response.keywords.map((keyword)=>{
+                        return keyword.text
+                    })
+                    resolve(keywords)
+                    
+                }
+            );
+        })
+        
     }
 }
 
